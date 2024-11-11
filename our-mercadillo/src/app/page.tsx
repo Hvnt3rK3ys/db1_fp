0b672a2c847a68bfab524fa1712be37f9fc6499b
carolinaguevara11 <145185790+carolinaguevara11@users.noreply.github.com>
2024-11-11 00:57:10 +0000

    UPDATE:primer intento

--- our-mercadillo/src/app/page.tsx
+++ our-mercadillo/src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Esta vaina sirve SIUUUU 🤭</li>
+          <li>se logro ome </li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
