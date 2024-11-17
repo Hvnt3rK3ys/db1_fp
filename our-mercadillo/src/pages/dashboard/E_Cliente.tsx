42da2a1a311a867e43a0f77cf13bf8e9bc6ef65f
Manuel Parra <81319513+Hvnt3rK3ys@users.noreply.github.com>
2024-11-17 00:39:40 +0000

    UPGRADE: gui and so on

--- our-mercadillo/src/pages/dashboard/E_Cliente.tsx
+++ our-mercadillo/src/pages/dashboard/E_Cliente.tsx
@@ -1,7 +1,76 @@
 import React from "react";
+import styles from "../../styles/E_dynamic.module.scss";
+import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, getKeyValue, Spinner, Button } from "@nextui-org/react";
+import { useAsyncList } from "@react-stately/data";
 
 const E_Cliente = () => {
-  return <div>E_Cliente</div>;
+  const [page, setPage] = React.useState(1);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  let list = useAsyncList({
+    async load({ signal, cursor }) {
+      if (cursor) {
+        setPage((prev) => prev + 1);
+      }
+
+      // If no cursor is available, then we're loading the first page.
+      // Otherwise, the cursor is the next URL to load, as returned from the previous page.
+      const res = await fetch(cursor || "https://swapi.py4e.com/api/people/?search=", { signal });
+      let json = await res.json();
+
+      if (!cursor) {
+        setIsLoading(false);
+      }
+
+      return {
+        items: json.results,
+        cursor: json.next,
+      };
+    },
+  });
+
+  const hasMore = page < 9;
+  return (
+    <section className={styles.globalContainer}>
+      <h1>E_Cliente</h1>
+
+      <Table
+        className={styles.globalTable}
+        isHeaderSticky
+        aria-label="Example table with client side sorting"
+        classNames={{
+          base: "max-h-[520px] overflow-y-scroll overflow-x-hidden", // Disable horizontal scroll
+          table: "min-h-[420px]",
+        }}
+      >
+        <TableHeader>
+          <TableColumn key="name" className={styles.tableHeader}>Name</TableColumn>
+          <TableColumn key="height" className={styles.tableHeader}>Height</TableColumn>
+          <TableColumn key="mass" className={styles.tableHeader}>Mass</TableColumn>
+          <TableColumn key="birth_year" className={styles.tableHeader}>Birth year</TableColumn>
+        </TableHeader>
+        <TableBody
+          isLoading={isLoading}
+          items={list.items}
+          loadingContent={<Spinner label="Loading..." />}
+        >
+          {(item) => (
+            <TableRow key={item.name}>
+              {(columnKey) => <TableCell className={styles.tableCell}>{getKeyValue(item, columnKey)}</TableCell>}
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+      {hasMore && !isLoading && (
+        <div className={styles.pagination}>
+          <Button isDisabled={list.isLoading} variant="flat" onPress={list.loadMore}>
+            {list.isLoading && <Spinner color="white" size="sm" />}
+            Load More
+          </Button>
+        </div>
+      )}
+    </section>
+  );
 };
 
 export default E_Cliente;
--- our-mercadillo/src/pages/dashboard/index.tsx
+++ our-mercadillo/src/pages/dashboard/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Layout from '../../components/layout/Layout';
 import styles from '../../styles/Dashboard.module.scss';
-import E_Bodega from './E_Bodega';
-import E_Cliente from './E_Cliente';
-import E_Empleado from './E_Empleado';
-import E_Exhibidor from './E_Exhibidor';
-import E_Factura from './E_Factura';
-import E_Producto from './E_Producto';
-import E_Proveedor from './E_Proveedor';
+import Image from 'next/image';
+import img_bodega from '../../public/covers/bodega.jpg';
+import img_cliente from '../../public/covers/cliente.jpg';
+import img_empleado from '../../public/covers/empleado.jpg';
+import img_exhibidor from '../../public/covers/exhibidor.jpg';
+import img_factura from '../../public/covers/factura.jpg';
+import img_producto from '../../public/covers/producto.jpg';
+import img_proveedor from '../../public/covers/proveedor.jpg';
 
 const Dashboard = () => {
   return (
@@ -18,25 +19,32 @@ const Dashboard = () => {
           
           <div className={styles.entities}>
             <div className={styles.entityCard}>
-              <E_Bodega />
+              <Image src={img_bodega} alt="Bodega" layout="fill" className={styles.cardImage} />
+              <h2>Bodega</h2>
             </div>
             <div className={styles.entityCard}>
-              <E_Cliente />
+              <Image src={img_cliente} alt="Cliente" layout="fill" className={styles.cardImage} />
+              <h2>Cliente</h2>
             </div>
             <div className={styles.entityCard}>
-              <E_Empleado />
+              <Image src={img_empleado} alt="Empleado" layout="fill" className={styles.cardImage} />
+              <h2>Empleado</h2>
             </div>
             <div className={styles.entityCard}>
-              <E_Exhibidor />
+              <Image src={img_exhibidor} alt="Exhibidor" layout="fill" className={styles.cardImage} />
+              <h2>Exhibidor</h2>
             </div>
             <div className={styles.entityCard}>
-              <E_Factura />
+              <Image src={img_factura} alt="Factura" layout="fill" className={styles.cardImage} />
+              <h2>Factura</h2>
             </div>
             <div className={styles.entityCard}>
-              <E_Producto />
+              <Image src={img_producto} alt="Producto" layout="fill" className={styles.cardImage} />
+              <h2>Producto</h2>
             </div>
             <div className={styles.entityCard}>
-              <E_Proveedor />
+              <Image src={img_proveedor} alt="Proveedor" layout="fill" className={styles.cardImage} />
+              <h2>Proveedor</h2>
             </div>
           </div>
         </div>
